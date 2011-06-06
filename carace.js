44b64ddd3035cab39077b3e7a473e52de5fbed92
Riley Davis <riley.davis@gmail.com>
2011-06-05 22:27:21 -0400

    added some protovis tests

---
+++ carace.js
@@ -0,0 +1,524 @@
+var popbyrace = [
+	{
+		"County": "Los Angeles",
+		"White": "4637062",
+		"Black": "930957",
+		"AmIndian": "76988",
+		"Asian": "1137500",
+		"PacIslander": "27053",
+		"Other": "2239997"
+	},
+	{
+		"County": "San Diego",
+		"White": "1871839",
+		"Black": "161480",
+		"AmIndian": "24337",
+		"Asian": "249802",
+		"PacIslander": "13561",
+		"Other": "360847"
+	},
+	{
+		"County": "Orange",
+		"White": "1844652",
+		"Black": "47649",
+		"AmIndian": "19906",
+		"Asian": "386785",
+		"PacIslander": "8938",
+		"Other": "421208"
+	},
+	{
+		"County": "Riverside",
+		"White": "1013478",
+		"Black": "96421",
+		"AmIndian": "18168",
+		"Asian": "56954",
+		"PacIslander": "3902",
+		"Other": "288868"
+	},
+	{
+		"County": "San Bernardino",
+		"White": "1006960",
+		"Black": "155348",
+		"AmIndian": "19915",
+		"Asian": "80217",
+		"PacIslander": "5110",
+		"Other": "355843"
+	},
+	{
+		"County": "Santa Clara",
+		"White": "905660",
+		"Black": "47182",
+		"AmIndian": "11350",
+		"Asian": "430095",
+		"PacIslander": "5773",
+		"Other": "204088"
+	},
+	{
+		"County": "Sacramento",
+		"White": "783240",
+		"Black": "121804",
+		"AmIndian": "13359",
+		"Asian": "134899",
+		"PacIslander": "7264",
+		"Other": "91541"
+	},
+	{
+		"County": "Alameda",
+		"White": "704334",
+		"Black": "215598",
+		"AmIndian": "9146",
+		"Asian": "295218",
+		"PacIslander": "9142",
+		"Other": "129079"
+	},
+	{
+		"County": "Contra Costa",
+		"White": "621490",
+		"Black": "88813",
+		"AmIndian": "5830",
+		"Asian": "103993",
+		"PacIslander": "3466",
+		"Other": "76510"
+	},
+	{
+		"County": "Ventura",
+		"White": "526721",
+		"Black": "14664",
+		"AmIndian": "7106",
+		"Asian": "40284",
+		"PacIslander": "1671",
+		"Other": "133178"
+	},
+	{
+		"County": "Fresno",
+		"White": "434045",
+		"Black": "42337",
+		"AmIndian": "12790",
+		"Asian": "64362",
+		"PacIslander": "1000",
+		"Other": "207061"
+	},
+	{
+		"County": "San Mateo",
+		"White": "420683",
+		"Black": "24840",
+		"AmIndian": "3140",
+		"Asian": "141684",
+		"PacIslander": "9403",
+		"Other": "71910"
+	},
+	{
+		"County": "Kern",
+		"White": "407581",
+		"Black": "39798",
+		"AmIndian": "9999",
+		"Asian": "22268",
+		"PacIslander": "972",
+		"Other": "153610"
+	},
+	{
+		"County": "San Francisco",
+		"White": "385728",
+		"Black": "60515",
+		"AmIndian": "3458",
+		"Asian": "239565",
+		"PacIslander": "3844",
+		"Other": "50368"
+	},
+	{
+		"County": "Sonoma",
+		"White": "374209",
+		"Black": "6522",
+		"AmIndian": "5389",
+		"Asian": "14098",
+		"PacIslander": "934",
+		"Other": "38717"
+	},
+	{
+		"County": "San Joaquin",
+		"White": "327607",
+		"Black": "37689",
+		"AmIndian": "6377",
+		"Asian": "64283",
+		"PacIslander": "1955",
+		"Other": "91613"
+	},
+	{
+		"County": "Stanislaus",
+		"White": "309901",
+		"Black": "11521",
+		"AmIndian": "5676",
+		"Asian": "18848",
+		"PacIslander": "1529",
+		"Other": "75187"
+	},
+	{
+		"County": "Santa Barbara",
+		"White": "290418",
+		"Black": "9195",
+		"AmIndian": "4784",
+		"Asian": "16344",
+		"PacIslander": "700",
+		"Other": "60683"
+	},
+	{
+		"County": "Monterey",
+		"White": "224682",
+		"Black": "15050",
+		"AmIndian": "4202",
+		"Asian": "24245",
+		"PacIslander": "1789",
+		"Other": "111782"
+	},
+	{
+		"County": "Solano",
+		"White": "222387",
+		"Black": "58827",
+		"AmIndian": "3110",
+		"Asian": "50299",
+		"PacIslander": "3078",
+		"Other": "31612"
+	},
+	{
+		"County": "Placer",
+		"White": "220053",
+		"Black": "2031",
+		"AmIndian": "2199",
+		"Asian": "7317",
+		"PacIslander": "386",
+		"Other": "8432"
+	},
+	{
+		"County": "Tulare",
+		"White": "213751",
+		"Black": "5852",
+		"AmIndian": "5737",
+		"Asian": "12018",
+		"PacIslander": "408",
+		"Other": "113317"
+	},
+	{
+		"County": "San Luis Obispo",
+		"White": "208699",
+		"Black": "5002",
+		"AmIndian": "2335",
+		"Asian": "6568",
+		"PacIslander": "286",
+		"Other": "15312"
+	},
+	{
+		"County": "Marin",
+		"White": "207800",
+		"Black": "7142",
+		"AmIndian": "1061",
+		"Asian": "11203",
+		"PacIslander": "388",
+		"Other": "11116"
+	},
+	{
+		"County": "Santa Cruz",
+		"White": "191931",
+		"Black": "2477",
+		"AmIndian": "2461",
+		"Asian": "8789",
+		"PacIslander": "382",
+		"Other": "38391"
+	},
+	{
+		"County": "Butte",
+		"White": "171728",
+		"Black": "2816",
+		"AmIndian": "3866",
+		"Asian": "6752",
+		"PacIslander": "296",
+		"Other": "9790"
+	},
+	{
+		"County": "Shasta",
+		"White": "145826",
+		"Black": "1225",
+		"AmIndian": "4528",
+		"Asian": "3048",
+		"PacIslander": "178",
+		"Other": "2790"
+	},
+	{
+		"County": "El Dorado",
+		"White": "140209",
+		"Black": "813",
+		"AmIndian": "1566",
+		"Asian": "3328",
+		"PacIslander": "209",
+		"Other": "5547"
+	},
+	{
+		"County": "Merced",
+		"White": "118350",
+		"Black": "8064",
+		"AmIndian": "2510",
+		"Asian": "14321",
+		"PacIslander": "396",
+		"Other": "55013"
+	},
+	{
+		"County": "Yolo",
+		"White": "114129",
+		"Black": "3425",
+		"AmIndian": "1953",
+		"Asian": "16614",
+		"PacIslander": "507",
+		"Other": "23214"
+	},
+	{
+		"County": "Humboldt",
+		"White": "107179",
+		"Black": "1111",
+		"AmIndian": "7241",
+		"Asian": "2091",
+		"PacIslander": "241",
+		"Other": "3099"
+	},
+	{
+		"County": "Napa",
+		"White": "99396",
+		"Black": "1645",
+		"AmIndian": "1045",
+		"Asian": "3694",
+		"PacIslander": "289",
+		"Other": "13604"
+	},
+	{
+		"County": "Nevada",
+		"White": "85948",
+		"Black": "259",
+		"AmIndian": "814",
+		"Asian": "715",
+		"PacIslander": "81",
+		"Other": "1782"
+	},
+	{
+		"County": "Madera",
+		"White": "76612",
+		"Black": "5072",
+		"AmIndian": "3212",
+		"Asian": "1566",
+		"PacIslander": "210",
+		"Other": "29979"
+	},
+	{
+		"County": "Imperial",
+		"White": "70290",
+		"Black": "5624",
+		"AmIndian": "2666",
+		"Asian": "2836",
+		"PacIslander": "119",
+		"Other": "55634"
+	},
+	{
+		"County": "Mendocino",
+		"White": "69671",
+		"Black": "536",
+		"AmIndian": "4103",
+		"Asian": "1038",
+		"PacIslander": "126",
+		"Other": "7427"
+	},
+	{
+		"County": "Kings",
+		"White": "69492",
+		"Black": "10747",
+		"AmIndian": "2178",
+		"Asian": "3980",
+		"PacIslander": "250",
+		"Other": "36611"
+	},
+	{
+		"County": "Sutter",
+		"White": "53291",
+		"Black": "1509",
+		"AmIndian": "1225",
+		"Asian": "8884",
+		"PacIslander": "161",
+		"Other": "10232"
+	},
+	{
+		"County": "Lake",
+		"White": "50289",
+		"Black": "1233",
+		"AmIndian": "1772",
+		"Asian": "482",
+		"PacIslander": "93",
+		"Other": "2398"
+	},
+	{
+		"County": "Tuolumne",
+		"White": "48750",
+		"Black": "1146",
+		"AmIndian": "992",
+		"Asian": "395",
+		"PacIslander": "91",
+		"Other": "1577"
+	},
+	{
+		"County": "Tehama",
+		"White": "47518",
+		"Black": "318",
+		"AmIndian": "1178",
+		"Asian": "440",
+		"PacIslander": "55",
+		"Other": "4631"
+	},
+	{
+		"County": "Yuba",
+		"White": "42537",
+		"Black": "1904",
+		"AmIndian": "1569",
+		"Asian": "4519",
+		"PacIslander": "123",
+		"Other": "5989"
+	},
+	{
+		"County": "Siskiyou",
+		"White": "38573",
+		"Black": "580",
+		"AmIndian": "1726",
+		"Asian": "526",
+		"PacIslander": "57",
+		"Other": "1224"
+	},
+	{
+		"County": "Calaveras",
+		"White": "36982",
+		"Black": "304",
+		"AmIndian": "705",
+		"Asian": "345",
+		"PacIslander": "38",
+		"Other": "839"
+	},
+	{
+		"County": "San Benito",
+		"White": "34695",
+		"Black": "573",
+		"AmIndian": "616",
+		"Asian": "1277",
+		"PacIslander": "99",
+		"Other": "13237"
+	},
+	{
+		"County": "Amador",
+		"White": "30113",
+		"Black": "1359",
+		"AmIndian": "626",
+		"Asian": "350",
+		"PacIslander": "36",
+		"Other": "1769"
+	},
+	{
+		"County": "Lassen",
+		"White": "27336",
+		"Black": "2992",
+		"AmIndian": "1104",
+		"Asian": "249",
+		"PacIslander": "146",
+		"Other": "1092"
+	},
+	{
+		"County": "Del Norte",
+		"White": "21693",
+		"Black": "1184",
+		"AmIndian": "1770",
+		"Asian": "637",
+		"PacIslander": "23",
+		"Other": "1079"
+	},
+	{
+		"County": "Plumas",
+		"White": "19113",
+		"Black": "130",
+		"AmIndian": "530",
+		"Asian": "110",
+		"PacIslander": "20",
+		"Other": "377"
+	},
+	{
+		"County": "Glenn",
+		"White": "18988",
+		"Black": "155",
+		"AmIndian": "552",
+		"Asian": "893",
+		"PacIslander": "35",
+		"Other": "4810"
+	},
+	{
+		"County": "Mariposa",
+		"White": "15234",
+		"Black": "114",
+		"AmIndian": "602",
+		"Asian": "122",
+		"PacIslander": "22",
+		"Other": "457"
+	},
+	{
+		"County": "Inyo",
+		"White": "14367",
+		"Black": "29",
+		"AmIndian": "1802",
+		"Asian": "163",
+		"PacIslander": "15",
+		"Other": "825"
+	},
+	{
+		"County": "Colusa",
+		"White": "12090",
+		"Black": "103",
+		"AmIndian": "439",
+		"Asian": "228",
+		"PacIslander": "74",
+		"Other": "5017"
+	},
+	{
+		"County": "Trinity",
+		"White": "11573",
+		"Black": "58",
+		"AmIndian": "631",
+		"Asian": "61",
+		"PacIslander": "15",
+		"Other": "114"
+	},
+	{
+		"County": "Mono",
+		"White": "10818",
+		"Black": "61",
+		"AmIndian": "309",
+		"Asian": "143",
+		"PacIslander": "11",
+		"Other": "1222"
+	},
+	{
+		"County": "Modoc",
+		"White": "8120",
+		"Black": "65",
+		"AmIndian": "398",
+		"Asian": "58",
+		"PacIslander": "7",
+		"Other": "538"
+	},
+	{
+		"County": "Sierra",
+		"White": "3348",
+		"Black": "7",
+		"AmIndian": "67",
+		"Asian": "6",
+		"PacIslander": "3",
+		"Other": "37"
+	},
+	{
+		"County": "Alpine",
+		"White": "890",
+		"Black": "7",
+		"AmIndian": "228",
+		"Asian": "4",
+		"PacIslander": "1",
+		"Other": "17"
+	}
+];
